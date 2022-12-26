4855a11b13ec65c79f33539238da8a6d737327ac
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-26 12:01:32 +0100

    add saving to localStorage

--- src/App.vue
+++ src/App.vue
@@ -133,7 +133,35 @@ export default {
     }
   },
 
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name);
+      })
+    }
+  },
+
   methods: {
+    subscribeToUpdates (tickerName) {
+      setInterval( async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=api_key`
+        );
+        const data = await f.json();
+        console.log('data', data)
+
+        this.tickers.find(t => t.name === tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD)
+        }
+      }, 3000);
+      this.ticker = "";
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -141,21 +169,9 @@ export default {
       };
 
       this.tickers.push(currentTicker);
-      setInterval( async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=your_api_key`
-        );
-        const data = await f.json();
-        console.log('data', data)
 
-        this.tickers.find(t => t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD)
-        }
-      }, 3000);
-
-      this.ticker = "";
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name)
     },
 
     handleDelete(tickerToRemove) {
