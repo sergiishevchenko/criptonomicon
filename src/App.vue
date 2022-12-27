b91a0613c978de9387dffa98e6539238f2d7ba7e
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-27 15:53:37 +0100

    graph resize

--- src/App.vue
+++ src/App.vue
@@ -93,7 +93,8 @@
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
           {{ selectedTicker.name }} - USD
         </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
+        <div class="flex items-end border-gray-600 border-b border-l h-64"
+             ref="graph">
           <div v-for="(bar, idx) in normalizedGraph"
                :key="idx"
                :style="{ height: `${bar}%` }"
@@ -146,6 +147,8 @@ export default {
       selectedTicker: null,
 
       graph: [],
+      maxGraphElements: 1,
+
       page: 1,
     }
   },
@@ -171,6 +174,14 @@ export default {
     }
 
     setInterval(this.updateTickers, 5000);
+  },
+
+  mounted() {
+    window.addEventListener("resize", this.calculateMaxGraphElements);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("resize", this.calculateMaxGraphElements);
   },
 
   computed: {
@@ -217,10 +228,21 @@ export default {
   },
 
   methods: {
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) {
+        return;
+      }
+
+      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
+    },
+
     updateTicker(tickerName, price) {
       this.tickers.filter(t => t.name === tickerName).forEach(t => {
         if (t === this.selectedTicker) {
           this.graph.push(price);
+          while (this.graph.length > this.maxGraphElements) {
+            this.graph.shift();
+          }
         }
         t.price = price;
       })
@@ -263,6 +285,7 @@ export default {
   watch: {
     selectedTicker() {
       this.graph = [];
+      this.$nextTick().then(this.calculateMaxGraphElements());
     },
 
     paginatedTickers() {
